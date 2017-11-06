302e919fb9f188eb25a954aabe31cfc2b94b8b80
Panos Karagiannis <panoskaragiannis.ucla@gmail.com>
2017-11-06 09:53:43 -0800

    added extra test cases and assert statements

--- homeworks/hw4/problem7/triangle.py
+++ homeworks/hw4/problem7/triangle.py
@@ -5,7 +5,7 @@ def isTriagle(a,b,c):
 	False otherwise '''
 
 	#take all pairs of two numbers and check
-	#that the third if their sum is larger 
+	#that the third is less than their sum  
 
 	return (a + b > c) and (a + c > b) and (b + c > a)
 
@@ -14,9 +14,22 @@ def isTriagle(a,b,c):
 
 def main():
 	# not a triangle since 8 + 4 < 15
+	assert isTriagle(8,15,4) == False
 	print(isTriagle(8,15,4))
 
+	# not a triangle since 8 + 7 < 16
+	assert isTriagle(16,7,8) == False
+	print(isTriagle(16,7,8))
+
+	# not a triangle since 1 + 7 < 9
+	assert isTriagle(1,7,9) == False
+	print(isTriagle(16,7,8))
+
+	assert isTriagle(3,6,4) == True
+	print(isTriagle(3,6,4))
+
 	# Pythagorean tiple 
+	assert isTriagle(3,4,5) == True
 	print(isTriagle(3,4,5))
 
 if __name__ == '__main__':
